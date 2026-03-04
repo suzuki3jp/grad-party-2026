1383f0425dc703dfd88f6bc70d10275493dd6d94
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2026-03-04 22:29:05 +0900

    画像を非公開に

--- src/constants/photos.ts
+++ src/constants/photos.ts
@@ -3,7 +3,7 @@ import { EVENT_END } from "./event";
 /**
  * これが true || EVENT_END が過ぎている時はモックを表示する
  */
-const GALLERY_MOCK_ENABLED = false;
+const GALLERY_MOCK_ENABLED = true;
 
 export function showMockGallery() {
   return GALLERY_MOCK_ENABLED || new Date() > EVENT_END;
